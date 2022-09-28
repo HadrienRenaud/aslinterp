c4bb7eab6bfe560825ca5c9e958589f611598b15
Hadrien Renaud <hadrien.renaud.22@ucl.ac.uk>
2022-09-28 15:59:00 +0100

    Replace compound values of ASL by Maps of CoreASL.

--- lib/values.ml
+++ lib/values.ml
@@ -1,7 +1,6 @@
-module Structure = Map.Make (String)
-
-type 'a structure = 'a Structure.t
 type bitstring = bool array
+type indexes = IInt of Z.t | IString of string
+type 'a map = (indexes * 'a) list
 
 type value =
   | Bitstr of bitstring
@@ -9,20 +8,19 @@ type value =
   | Real of Q.t
   | Bool of bool
   | String of string
-  | Enum of string
-    (* as a named value cannot be shared between different enumerations, we do not have to specify of which enumeration we are talking. *)
-  | Struct of value structure
-  | Tuple of value array
-  | Array of value array
+  | Map of value map
 
 module F = Format
 
 let pp_print_bit f b = F.pp_print_int f (if b then 1 else 0)
-let make_cutter sep f () = F.fprintf f sep
 
-let rec pp_print_struct_elem f e =
-  let k, v = e in
-  F.fprintf f "@[<2>\"%s\":@ %a@]" k pp_print_value v
+let pp_print_indexes f i =
+  match i with
+  | IInt z -> Z.pp_print f z
+  | IString s -> F.fprintf f "@[<h>'%s'@]" s
+
+let rec pp_print_map_element f = function
+  | i, v -> F.fprintf f "@[<h>%a:@ %a@]" pp_print_indexes i pp_print_value v
 
 and pp_print_value f v =
   match v with
@@ -30,23 +28,16 @@ and pp_print_value f v =
   | Int x -> Z.pp_print f x
   | Bool b -> F.pp_print_bool f b
   | String s -> F.fprintf f "@[<h>\"%s\"@]" s
-  | Enum s -> F.pp_print_string f s
   | Bitstr a ->
       F.fprintf f "@[<h>b\"%a\"@]"
-        (F.pp_print_seq ~pp_sep:(make_cutter "") pp_print_bit)
+        (F.pp_print_seq ~pp_sep:F.pp_print_cut pp_print_bit)
         (Array.to_seq a)
-  | Struct s ->
-      F.fprintf f "@[<hv 2>{ %a@] }"
-        (F.pp_print_seq ~pp_sep:(make_cutter ",@ ") pp_print_struct_elem)
-        (Structure.to_seq s)
-  | Tuple a ->
-      F.fprintf f "@[<hv 2>( %a )@]"
-        (F.pp_print_seq ~pp_sep:(make_cutter ",@ ") pp_print_value)
-        (Array.to_seq a)
-  | Array a ->
+  | Map l ->
       F.fprintf f "@[<hv 2>[ %a ]@]"
-        (F.pp_print_seq ~pp_sep:(make_cutter ",@ ") pp_print_value)
-        (Array.to_seq a)
+        (F.pp_print_list
+           ~pp_sep:(fun f () -> F.fprintf f ", @")
+           pp_print_map_element)
+        l
 
 let bitstring_of_z n l =
   let add_trailing_zeros s = Seq.append s (Seq.repeat false) in
