adec6ccb8d056137c8552daab7cf77aa67ae8075
Hadrien Renaud <hadrien.renaud.22@ucl.ac.uk>
2022-09-28 15:59:00 +0100

    Replace compound values of ASL by Maps of CoreASL.

--- lib/values.mli
+++ lib/values.mli
@@ -1,23 +1,20 @@
-module Structure : Map.S with type key = string
-
-type 'a structure = 'a Structure.t
 type bitstring = bool array
 
-(** Values from the spec, section K16.3 *)
+(** Type of indexing values, either in ASL tuples, ASL arrays or ASL structures. *)
+type indexes = IInt of Z.t | IString of string
+
+type 'a map = (indexes * 'a) list
+(** Underlying object behind a Map. It is for now an association list. *)
+
+(** Values in CoreASL, ie values from ASL, where compound values are mapped to Map. *)
 type value =
   | Bitstr of bitstring  (** Bit strings of fixed size *)
   | Int of Z.t  (** Integers, unbounded in size, signed *)
   | Real of Q.t
       (** Real number in the mathematical sense, unbounded in size or precision *)
-  (* TODO: investigate if there is a possibility for real number with unbounded precision in caml *)
   | Bool of bool  (** Boolean *)
   | String of string  (** Strings *)
-  | Enum of string  (** Enumeration, ie a member of a set of named values *)
-    (* as a named value cannot be shared between different enumerations, we do not have to specify of which enumeration we are talking. *)
-  | Struct of value structure
-      (** Structures: a compoud value composed of one or more value items *)
-  | Tuple of value array  (** Tuples, an ordered set of values *)
-  | Array of value array
+  | Map of value map  (** Maps *)
 
 val pp_print_value : Format.formatter -> value -> unit
 val bitstring_of_z : Z.t -> int -> bitstring
