4972c98a158a68d1599cfbfa7527ed12d0259c42
Hadrien Renaud <hadrien.renaud.22@ucl.ac.uk>
2022-10-06 00:00:47 +0100

    Re-use cas example from before

--- bin/main.ml
+++ bin/main.ml
@@ -19,16 +19,34 @@ let run_and_print = function
       Format.printf "@\n-------------------------\n\n@\n";
       Format.eprintf "@\n"
 
-let () =
-  let s =
-    stmt_from_list
+let cas =
+  ( stmt_from_list
       [
-        SAssign (LEVar "x", ELiteral (make_int 3));
-        SAssign (LEVar "y", ELiteral (make_int 5));
+        SAssign (LEVar "comparevalue", EGetArray (EVar "X", EVar "s"));
+        SAssign (LEVar "newvalue", EGetArray (EVar "X", EVar "t"));
+        SAssign (LEVar "address", EGetArray (EVar "X", EVar "n"));
+        SAssign (LEVar "oldvalue", EGetArray (EVar "Mem", EVar "address"));
         SCond
-          ( EBinop (EVar "x", Eq, EVar "y"),
-            SAssign (LEVar "r", EBinop (EVar "x", Plus, EVar "y")),
-            SAssign (LEVar "r", EBinop (EVar "x", Minus, EVar "y")) );
-      ]
-  in
-  run_and_print (s, C.empty)
+          ( EBinop (EVar "comparevalue", Eq, EVar "oldvalue"),
+            SAssign (LESetArray (LEVar "Mem", EVar "address"), EVar "newvalue"),
+            SPass );
+        SAssign (LESetArray (LEVar "X", EVar "s"), EVar "oldvalue");
+      ],
+    let address = Z.of_int 12345 in
+    let s = Z.of_int 1 in
+    let t = Z.of_int 2 in
+    let n = Z.of_int 3 in
+    let old_value = make_int 1 in
+    let compare_value = make_int 1 in
+    let new_value = make_int 2 in
+    let memory = VArray [ (address, old_value) ] in
+    let x = VArray [ (s, compare_value); (t, new_value); (n, VInt address) ] in
+    let c = C.empty in
+    let c = Result.get_ok @@ C.set "s" [] (VInt s) c in
+    let c = Result.get_ok @@ C.set "t" [] (VInt t) c in
+    let c = Result.get_ok @@ C.set "n" [] (VInt n) c in
+    let c = Result.get_ok @@ C.set "Mem" [] memory c in
+    let c = Result.get_ok @@ C.set "X" [] x c in
+    c )
+
+let () = run_and_print cas
