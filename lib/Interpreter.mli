feba5228c5287017e6dd22134047f6fcd32fd571
Hadrien Renaud <hadrien.renaud2@arm.com>
2022-11-06 12:13:04 +0000

    Add possibility to pass ocaml functions into asl

--- lib/Interpreter.mli
+++ lib/Interpreter.mli
@@ -2,8 +2,10 @@ module type S = sig
   module B : Backend.S
 
   type ast = B.value AST.t
+  type sfunc = B.value list -> B.value list B.m
 
-  val run : ast -> B.value list -> B.value list B.m
+  val run : ast -> (string * sfunc) list -> B.value list -> B.value list B.m
+  (** [run spec_lib ast args] runs the function main of the ast, in an environment build from the ast and spec_lib. *)
 end
 
 module Make (B : Backend.S) : S with module B = B
